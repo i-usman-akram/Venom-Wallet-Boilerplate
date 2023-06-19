4b16a0ec031e2b3e1b9d731af0b89e5625c19848
Muhammad Usman Akram <93033145+i-usman-akram@users.noreply.github.com>
2023-06-19 22:57:55 +0500

    update ever wallet

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,123 +1,133 @@
-import { Address, ProviderRpcClient  } from "everscale-inpage-provider"
-import React, { useEffect, useState } from "react"
-import { EverscaleStandaloneClient,EverWalletAccount, SimpleKeystore } from "everscale-standalone-client"
+import { Address, ProviderRpcClient } from "everscale-inpage-provider";
+import React, { useEffect, useState } from "react";
+import { EverscaleStandaloneClient, EverWalletAccount, SimpleKeystore, AccountsStorage } from "everscale-standalone-client";
 // import {EverWalletAccount} from "@eversdk/core";
-import BackImg from "../styles/img/decor.svg"
-import LogOutImg from "../styles/img/log_out.svg"
+import BackImg from "../styles/img/decor.svg";
+import LogOutImg from "../styles/img/log_out.svg";
 
 // Importing of our contract ABI from smart-contract build action. Of cource we need ABI for contracts calls.
 // import tokenRootAbi from "../abi/TokenRoot.abi.json"
 // import tokenWalletAbi from "../abi/TokenWallet.abi.json"
+import tokenWalletAbi from "../abi/TokenWallet.abi.json";
+import tokenRootAbi from "../abi/TokenRoot.abi.json";
 
-import ConnectWallet from "../components/ConnectWallet"
-import Transfer from "../components/Transfer"
+import ConnectWallet from "../components/ConnectWallet";
+import Transfer from "../components/Transfer";
 import { abi, IR_CONTRACT_ADDRESS } from "../constants";
 let keyPair = {
   publicKey: "0eabed4ad0687ede4d103dda14cc582238237362c2d363e28e73c914181d2c7a",
-  secretKey: "1552f761f617b7a2cec21ca6bc654fbf2632f9f6cc3ac4c27bbaf8455d8f8239"
-}
+  secretKey: "1552f761f617b7a2cec21ca6bc654fbf2632f9f6cc3ac4c27bbaf8455d8f8239",
+};
 
 let eas = await EverWalletAccount.fromPubkey({publicKey:"0eabed4ad0687ede4d103dda14cc582238237362c2d363e28e73c914181d2c7a"})
+console.log(eas);
+
 let easAddress = "0:a3457cb4910926ebbd181a16077951c54ba2bbbe1769da02ff5b5830182de9c5"
+// const account = new EverWalletAccount(new Address(easAddress))
+// console.log(account)
+// eas["getAccount"] = (easAddress) => {
+  // return account
+// }
 
-eas["getAccount"] = (address) => {
-  return eas
-}
-
-
-let sks = new SimpleKeystore()
-sks.addKeyPair(keyPair.publicKey, keyPair)
+let sks = new SimpleKeystore();
+sks.addKeyPair(keyPair.publicKey, keyPair);
 
 const ever = new ProviderRpcClient({
   fallback: () =>
-      EverscaleStandaloneClient.create({
-          connection: {
-              id: 1, // network id
-              type: 'graphql',
-              data: {
-                  endpoints: ['gql-testnet.venom.foundation'],
-              },
-          },
-          keystore: sks,
-          accountsStorage: eas,
-
-      }),
+    EverscaleStandaloneClient.create({
+      connection: {
+        id: 1002,
+        group: "venom_testnet",
+        type: "jrpc",
+        data: {
+          endpoint: "https://jrpc-testnet.venom.foundation/rpc",
+        },
+      },
+      keystore: sks,
+      accountsStorage: eas,
+    }),
 });
-
 
 await ever.ensureInitialized();
 
 await ever.requestPermissions({
-  permissions: ['basic'],
+  permissions: ["basic"],
 });
 function Main({ venomConnect }) {
-  const [venomProvider, setVenomProvider] = useState()
-  const [address, setAddress] = useState()
+  const [venomProvider, setVenomProvider] = useState();
+  const [address, setAddress] = useState();
 
   // We will store token balance from contract
-  const [balance, setBalance] = useState()
-  let tokenWalletAddress // User's TIP-3 TokenWallet address
+  const [balance, setBalance] = useState();
+  let tokenWalletAddress; // User's TIP-3 TokenWallet address
 
   // This method allows us to gen a wallet address from inpage provider
   const getAddress = async provider => {
-    const providerState = await provider?.getProviderState?.()
-    return providerState?.permissions.accountInteraction?.address.toString()
-  }
+    const providerState = await provider?.getProviderState?.();
+    return providerState?.permissions.accountInteraction?.address.toString();
+  };
 
   // Any interaction with venom-wallet (address fetching is included) needs to be authentificated
   const checkAuth = async _venomConnect => {
-    const auth = await _venomConnect?.checkAuth()
-    if (auth) await getAddress(_venomConnect)
-  }
+    const auth = await _venomConnect?.checkAuth();
+    if (auth) await getAddress(_venomConnect);
+  };
 
   // This handler will be called after venomConnect.login() action
   // connect method returns provider to interact with wallet, so we just store it in state
   const onConnect = async provider => {
-    setVenomProvider(provider)
-    await onProviderReady(provider)
-  }
+    setVenomProvider(provider);
+    await onProviderReady(provider);
+  };
 
   // This handler will be called after venomConnect.disconnect() action
   // By click logout. We need to reset address and balance.
   const onDisconnect = async () => {
-    venomProvider?.disconnect()
-    setAddress(undefined)
-  }
+    venomProvider?.disconnect();
+    setAddress(undefined);
+  };
 
   // When our provider is ready, we need to get address and balance from.
   const onProviderReady = async provider => {
-    const venomWalletAddress = provider ? await getAddress(provider) : undefined
-    setAddress(venomWalletAddress)
-  }
+    const venomWalletAddress = provider ? await getAddress(provider) : undefined;
+    setAddress(venomWalletAddress);
+  };
 
   useEffect(() => {
     // connect event handler
-    const off = venomConnect?.on("connect", onConnect)
+    const off = venomConnect?.on("connect", onConnect);
     if (venomConnect) {
-      checkAuth(venomConnect)
+      checkAuth(venomConnect);
     }
     // just an empty callback, cuz we don't need it
     return () => {
-      off?.()
-    }
-  }, [venomConnect])
+      off?.();
+    };
+  }, [venomConnect]);
 
-  const callthis = async ()=>{
-    const contr = new ever.Contract(abi, IR_CONTRACT_ADDRESS);
-    await contr.methods.transfer({
-      amount: 1 * 18,
-      recipient: new Address("0:344e58679c5a91513a7ec1dc590c77db1f7becddf818fde42d4dd907ede056bc"),
-      deployWalletValue: 0,
-      remainingGasTo: new Address("0:a3457cb4910926ebbd181a16077951c54ba2bbbe1769da02ff5b5830182de9c5"),
-      notify: false,
-      payload: ''
-    }).send({
-      from: new Address("0:a3457cb4910926ebbd181a16077951c54ba2bbbe1769da02ff5b5830182de9c5"), 
-      recipient: new Address("0:344e58679c5a91513a7ec1dc590c77db1f7becddf818fde42d4dd907ede056bc"),
-      amount: '1000000000'
-    });
+  const callthis = async () => {
+    const contr = new ever.Contract(
+      tokenWalletAbi,
+      "0:1c62d3893eb2f524620eb151f0e2474db34786f791cb17087ce175c5a377089e",
+    );
+    console.log(contr);
+    const result = await contr.methods
+      .transfer({
+        amount: "1000000000000000000",
+        recipient: "0:344e58679c5a91513a7ec1dc590c77db1f7becddf818fde42d4dd907ede056bc",
+        deployWalletValue: "100000000",
+        remainingGasTo: "0:a3457cb4910926ebbd181a16077951c54ba2bbbe1769da02ff5b5830182de9c5",
+        notify: false,
+        payload: "",
+      })
+      .sendExternal({
+        withoutSignature: true,
+        from:  "0:a3457cb4910926ebbd181a16077951c54ba2bbbe1769da02ff5b5830182de9c5",
+        amount: "1000000000",
+      });
 
-  }
+    console.log(result);
+  };
 
   return (
     <div className="box">
@@ -133,17 +143,17 @@ function Main({ venomConnect }) {
       <img className="decor" src={BackImg} alt="back" />
       <div className="card">
         <div className="card__wrap">
-          {address ?<Transfer callthis={callthis}/>
-           : (
+          {address ? (
+            <Transfer callthis={callthis} />
+          ) : (
             <>
-            <ConnectWallet venomConnect={venomConnect} />
+              <ConnectWallet venomConnect={venomConnect} />
             </>
           )}
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Main
-
+export default Main;
